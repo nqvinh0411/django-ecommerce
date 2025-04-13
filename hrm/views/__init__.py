1b7738f6b7177b3701b98dd04dccae0545caf532
nqvinh0411 <openai041195@gmail.com>
2025-04-13 19:34:41 +0700

    Add migrations

--- hrm/views/__init__.py
+++ hrm/views/__init__.py
@@ -1,21 +1,30 @@
-from .department_view import DepartmentViewSet
-from .position_view import PositionViewSet
-from .employee_view import EmployeeViewSet
-from .work_schedule_view import WorkScheduleViewSet
-from .holiday_view import HolidayViewSet
-from .timesheet_view import TimesheetViewSet
-from .leave_request_view import LeaveRequestViewSet
-from .salary_view import SalaryViewSet
-from .payroll_view import PayrollViewSet
+from .department_view import DepartmentListView, DepartmentCreateView, DepartmentRetrieveUpdateDestroyView
+from .position_view import PositionListView, PositionCreateView, PositionRetrieveUpdateDestroyView
+from .employee_view import EmployeeListView, EmployeeCreateView, EmployeeRetrieveUpdateDestroyView
+from .work_schedule_view import WorkScheduleListView, WorkScheduleCreateView, WorkScheduleRetrieveUpdateDestroyView
+from .holiday_view import HolidayListView, HolidayCreateView, HolidayRetrieveUpdateDestroyView
+from .timesheet_view import TimesheetListView, TimesheetCreateView, TimesheetRetrieveUpdateDestroyView
+from .leave_request_view import LeaveRequestListView, LeaveRequestCreateView, LeaveRequestRetrieveUpdateDestroyView
+from .salary_view import SalaryListView, SalaryCreateView, SalaryRetrieveUpdateDestroyView
+from .payroll_view import PayrollListView, PayrollCreateView, PayrollRetrieveUpdateDestroyView
 
 __all__ = [
-    'DepartmentViewSet',
-    'PositionViewSet',
-    'EmployeeViewSet',
-    'WorkScheduleViewSet',
-    'HolidayViewSet',
-    'TimesheetViewSet',
-    'LeaveRequestViewSet',
-    'SalaryViewSet',
-    'PayrollViewSet'
+    # Department views
+    'DepartmentListView', 'DepartmentCreateView', 'DepartmentRetrieveUpdateDestroyView',
+    # Position views
+    'PositionListView', 'PositionCreateView', 'PositionRetrieveUpdateDestroyView',
+    # Employee views
+    'EmployeeListView', 'EmployeeCreateView', 'EmployeeRetrieveUpdateDestroyView',
+    # WorkSchedule views
+    'WorkScheduleListView', 'WorkScheduleCreateView', 'WorkScheduleRetrieveUpdateDestroyView',
+    # Holiday views
+    'HolidayListView', 'HolidayCreateView', 'HolidayRetrieveUpdateDestroyView',
+    # Timesheet views
+    'TimesheetListView', 'TimesheetCreateView', 'TimesheetRetrieveUpdateDestroyView',
+    # LeaveRequest views
+    'LeaveRequestListView', 'LeaveRequestCreateView', 'LeaveRequestRetrieveUpdateDestroyView',
+    # Salary views
+    'SalaryListView', 'SalaryCreateView', 'SalaryRetrieveUpdateDestroyView',
+    # Payroll views
+    'PayrollListView', 'PayrollCreateView', 'PayrollRetrieveUpdateDestroyView'
 ]
